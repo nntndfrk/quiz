cba898e77ba4b8e1b1e1140f30ea5a003ae25cb6
nntndfrk <nntndfrk@gmail.com>
2017-09-03 22:57:13 +0300

    My solution quiz

--- js/app.js
+++ js/app.js
@@ -1,33 +1,71 @@
 class App {
     constructor({ element, quiz }) {
-        
+        this.quiz = quiz;
+        this.mainContainer = element;
+        this.marks = ['Неудача.', 'Хм...', 'Удовлетворительно.', 'Неплохо.', 'Хорошо!', 'Отлично!!!'];
+
+        this.init();
     }
 
     init() {
+        this.quizContainer = document.querySelector('#quiz');
+        this.title = document.querySelector('#title');
+        this.score = document.querySelector('#score');
+        this.question = document.querySelector('#question');
+        this.answerList = document.querySelector('#answers');
+        this.progressBar = document.querySelector('#progress');
 
+        this.answerList.addEventListener('click', this.handleAnswerButtonClick.bind(this));
+    }
+
+    _removeChildren(elem) {
+        while (elem.lastChild) {
+            elem.removeChild(elem.lastChild);
+        }
     }
 
     handleAnswerButtonClick({ target }) {
-
+        this.quiz.checkAnswer(target.innerText);
+        if (this.quiz.step < this.quiz.totalQuiestions) {
+            this.displayNext();
+        } else {
+            this.displayScore();
+        }
     }
 
     displayNext() {
-
+        this.title.innerHTML = this.quiz.title;
+        this.displayQuestion();
+        this.displayAnswers();
+        this.displayProgress();
     }
 
     displayQuestion() {
-        
+        this.question.innerHTML = this.quiz.currentQuestion.text;
     }
 
     displayAnswers() {
-        
+        let answers = this.quiz.currentQuestion.answers;
+        this._removeChildren(this.answerList);
+        for (let i = 0; i <  answers.length; i++) {
+            let li = document.createElement('li');
+            li.innerHTML = answers[i];
+            li.classList.add('list-group-item', 'list-group-item-action');
+            this.answerList.appendChild(li);
+        }
     }
 
+
     displayProgress() {
-        
+        this.progressBar.textContent = `Вопрос ${this.quiz.step+1} из ${this.quiz.totalQuiestions}`;
     }
 
     displayScore() {
-        
+        this.title.style.display = "none";
+        this.question.style.display = "none";
+        this.answerList.style.display = "none";
+        this.progressBar.style.display = "none";
+        this.score.innerHTML = `${this.marks[this.quiz.correctAnswers]} Правильных ответов: ${this.quiz.correctAnswers}`;
+
     }
 }
--- js/quiz.js
+++ js/quiz.js
@@ -1,17 +1,37 @@
 class Quiz {
     constructor(title, quiestions) {
+        this.title = title;
+        this._quiestions = quiestions;
+        this._counter = 0;
+        this._correctAnswersCounter = 0;
+    }
 
+    get step() {
+        return this._counter;
+    }
+
+    get totalQuiestions() {
+        return this._quiestions.length;
+    }
+
+    get correctAnswers() {
+        return this._correctAnswersCounter;
     }
 
     get currentQuestion() {
+        if (this._counter < this._quiestions.length) {
+            return this._quiestions[this.step];
+        }
         
     }
-
+    
     get hasEnded() {
-        
+        return (this._counter < this._quiestions.length) ? false : true;
     }
-
+    
     checkAnswer(answer) {
-        
+        let correctAnswer = this._quiestions[this.step]['answers'][this._quiestions[this._counter]['correctAnswer']]
+        this._counter++;
+        if ( answer === correctAnswer ) this._correctAnswersCounter++;
     }
 }
